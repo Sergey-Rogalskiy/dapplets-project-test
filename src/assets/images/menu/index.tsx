a84cb70d957c43ebd59581cb2b15d03c5806959d
Sergey-Rogalskiy <rogalskiy.sergey@gmail.com>
2021-07-30 02:38:22 +0300

    feat: sidenav

---
+++ src/assets/images/menu/index.tsx
@@ -0,0 +1,13 @@
+import AllLogo from './all.svg'
+import EditorLogo from './editor.svg'
+import EssentialLogo from './essential.svg'
+import SocialLogo from './social.svg'
+import FinancialLogo from './financial.svg'
+
+export {
+    AllLogo,
+    EditorLogo,
+    EssentialLogo,
+    SocialLogo,
+    FinancialLogo,
+}
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,35 +1,59 @@
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import Tags from '../../components/Tags/Tags';
 import DappletsProjectImage from '../../assets/images/dapplets-project-image.svg';
+import {
+  AllLogo,
+  EditorLogo,
+  EssentialLogo,
+  SocialLogo,
+  FinancialLogo,
+} from '../../assets/images/menu';
+
 import s from './sidebar.module.css';
 
 function Sidebar() {
   return (
     <div className={s.sidebar}>
       <Link to='./'>
-        <div>
           <img src={DappletsProjectImage} alt='-'/>
           <span>Dapplets Project.</span>
-        </div>
       </Link>
 
-      <div className={s.main_menu}>
-        <Link to='./dapplets'>Editor’s Choice</Link>
-        <Link to='./dapplets'>Essential Dapplets</Link>
-        <Link to='./dapplets'>Social Networks</Link>
-        <Link to='./dapplets'> Dapplets</Link>
+      <div className={s.menu}>
+        <NavLink to='./dapplets' activeClassName={s.a_active}>
+            <img src={AllLogo} alt='-'/>
+            <span>All Dapplets</span>
+        </NavLink>
+        <Link to='./dapplets'>
+            <img src={EditorLogo} alt='-'/>
+            <span>Editor’s Choice</span>
+        </Link>
+        <Link to='./dapplets'>
+            <img src={EssentialLogo} alt='-'/>
+            <span>Essential Dapplets</span>
+        </Link>
+        <Link to='./dapplets'>
+            <img src={SocialLogo} alt='-'/>
+            <span>Social Networks</span>
+        </Link>
+        <Link to='./dapplets'>
+            <img src={FinancialLogo} alt='-'/>
+            <span>Financial Dapplets</span>
+        </Link>
       </div>
 
-      <div className={s.list_menu}>
-        <p>My lists</p>
+      <div className={`${s.menu} ${s.list}`}>
+        <p className={s.title}>My lists</p>
         <Link to='./dapplets'>TOP-10 Twitter Dapplets (Me)</Link>
         <Link to='./dapplets'>Best Financial dapplets by Jack (Jack)</Link>
         <Link to='./dapplets'>TOP-10 Twitter Dapplets (Me)</Link>
       </div>
 
-      <div>
-        <p>My Tags</p>
-        <Tags />
+      <div className={`${s.menu}`}>
+        <p className={s.title}>My Tags</p>
+        <div className={`${s.tags}`}>
+          <Tags />
+        </div>
       </div>
       
     </div>
